9a083d6dd8f0169d9d45c5db0e94b198b6db9a4e
Your Name <jason@git.training>
2023-11-07 12:52:19 +0530

    by suchita

---
+++ Tags.bat
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title> Some imporatant tags</title>
+    </head>
+    <body>
+        <b>tThis is bold tag</b><br>
+        <strong> This is strong tag</strong><br>
+        <i>This is italic tag</i><br>s>blue</ins></p><br>
+        <p>H<sub>2</sub>o</P>
+        <p> a <sup>2</sup></p>
+        <p>a<sup>2</sup>+b<sup>2</sup>+c <sup>2</sup></p>
+
+            
+
+        
+        </body></html>
